546799d90ab416b48bb03d5215d7d16e30e1732f
Bilal <bsajid173@gmail.com>
2022-11-09 22:28:24 +0530

    navbar tweaks

--- src/components/Navbar.js/navbar.js
+++ src/components/Navbar.js/navbar.js
@@ -2,6 +2,12 @@ import { Fragment, useState } from "react";
 import LightModeIcon from "@mui/icons-material/LightMode";
 import DarkModeIcon from "@mui/icons-material/DarkMode";
 import MenuIcon from "@mui/icons-material/Menu";
+import PersonIcon from "@mui/icons-material/Person";
+import CloseIcon from "@mui/icons-material/Close";
+import LightbulbIcon from "@mui/icons-material/Lightbulb";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
+import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
+import HomeIcon from "@mui/icons-material/Home";
 import classes from "./navbar.module.css";
 import { modeActions } from "../../store/darkmode";
 import { useSelector, useDispatch } from "react-redux";
@@ -50,6 +56,10 @@ const Navbar = () => {
         <div className="w-fit font-serif text-3xl px-2 font-black mr-auto text-gray-700 dark:text-white">
           Bilal Sajid
         </div>
+        <button onClick={darkModeHandler} className="p-1 mr-2 dark:text-white">
+          {!isDark && <LightModeIcon />}
+          {isDark && <DarkModeIcon />}
+        </button>
         <div
           className={`${classes.rightdiv} hidden md:flex w-fit flex-wrap justify-center align-middle text-gray-700 dark:text-white`}
         >
@@ -69,43 +79,44 @@ const Navbar = () => {
             Contact
           </button>
         </div>
-        <div className="md:hidden flex w-fit flex-wrap ml-auto justify-center align-middle  text-gray-700 dark:text-white">
-          <button onClick={darkModeHandler} className="p-1">
-            {!isDark && <LightModeIcon />}
-            {isDark && <DarkModeIcon />}
-          </button>
-          <button onClick={showMenuHandler}>
-            <MenuIcon />
-          </button>
+        <div className="md:hidden flex w-fit flex-wrap justify-center align-middle  text-gray-700 dark:text-white">
+          {!showMenu ? (
+            <button
+              onClick={showMenuHandler}
+              className="transition-all duration-200"
+            >
+              <MenuIcon />
+            </button>
+          ) : (
+            <button
+              onClick={showMenuHandler}
+              className="transition-all duration-200"
+            >
+              <CloseIcon />
+            </button>
+          )}
         </div>
       </div>
-
       <div className="h-[120vh]"></div>
-      {showMenu && (
-        <div
-          onClick={showMenuHandler}
-          className="fixed top-0 left-0 w-full h-screen z-10 bg-gray-900 opacity-50"
-        ></div>
-      )}
       <div
         className={`duration-300 ease-in-out transition-all transform ${
-          showMenu ? "translate-x-0" : "translate-x-full"
-        } py-3 flex flex-col w-9/12 top-0 right-0 fixed h-screen bg-white z-20 ml-auto dark:bg-gray-800 text-gray-700 dark:text-white`}
+          showMenu ? "-translate-x-0" : "-translate-x-full"
+        } py-3 flex flex-col w-[4rem] md:hidden items-center justify-center top-56 opacity-60 left-0 fixed h-[18rem] bg-white z-20 ml-auto dark:bg-gray-900 text-gray-700 dark:text-white`}
       >
-        <div className="px-2 text-right text-lg">
-          {/* <CloseIcon onClick={showMenuHandler} /> */}
-        </div>
-        <button className="transition-all duration-300 px-2 py-1 my-1 text-left hover:bg-blue-600 hover:text-white">
-          {/* <LoginIcon className="mr-2" /> Login */}
+        <button className="transition-all duration-300 px-2 py-1 my-1 text-left hover:text-white">
+          <HomeIcon fontSize="large" />
         </button>
-        <button className="transition-all duration-300 my-1 text-left px-2 py-1 hover:bg-blue-600 hover:text-white">
-          {/* <AddBoxIcon className="mr-2" /> Create Account */}
+        <button className="transition-all duration-300 my-1 text-left px-2 py-1 hover:text-white">
+          <PersonIcon fontSize="large" />
         </button>
-        <button className="transition-all duration-300 my-1 text-left px-2 py-1 hover:bg-blue-600 hover:text-white">
-          {/* <AddBoxIcon className="mr-2" /> New Article */}
+        <button className="transition-all duration-300 my-1 text-left px-2 py-1 hover:text-white">
+          <LightbulbIcon fontSize="large" />
         </button>
-        <button className="transition-all duration-300 my-1 text-left px-2 py-1 hover:bg-blue-600 hover:text-white">
-          {/* <SearchIcon className="mr-2" /> Search */}
+        <button className="transition-all duration-300 my-1 text-left px-2 py-1 hover:text-white">
+          <RemoveRedEyeIcon fontSize="large" />
+        </button>
+        <button className="transition-all duration-300 my-1 text-left px-2 py-1 hover:text-white">
+          <MailOutlineIcon fontSize="large" />
         </button>
       </div>
     </Fragment>
