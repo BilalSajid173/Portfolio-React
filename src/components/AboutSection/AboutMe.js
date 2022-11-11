4c7c044f0088f39f8a9bff6ac940598644ce9687
Bilal <bsajid173@gmail.com>
2022-11-11 17:46:50 +0530

    Fix with waves div being relative with 0 height

---
+++ src/components/AboutSection/AboutMe.js
@@ -0,0 +1,14 @@
+const AboutMe = () => {
+  return (
+    <>
+      <div className="flex flex-wrap justify-center py-10">
+        <div>
+          <h1 className="font-bold text-2xl">About Me</h1>
+        </div>
+        <div></div>
+      </div>
+    </>
+  );
+};
+
+export default AboutMe;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,13 @@
 import Navbar from "../components/Navbar.js/navbar";
 import TopSection from "../components/TopSection/TopPart";
+import AboutMe from "../components/AboutSection/AboutMe";
 
 const Home = () => {
   return (
     <>
       <Navbar />
       <TopSection />
+      <AboutMe />
     </>
   );
 };
