abf2993ed70c212d4cea099090dc3ccf6b40249e
Bilal <bsajid173@gmail.com>
2022-11-13 02:21:50 +0530

    progress bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import Home from "./pages/HomePage";
 import "animate.css";
 import { ToastContainer } from "react-toastify";
+import { motion, useScroll, useSpring } from "framer-motion";
 
 function App() {
+  const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001,
+  });
   return (
     <>
+      <motion.div className="progress-bar" style={{ scaleX }} />
       <Home />
       <ToastContainer />
     </>
