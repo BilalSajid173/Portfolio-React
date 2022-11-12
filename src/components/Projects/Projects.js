aa79004b09c58eae39d233605e573a53a09ae9b9
Bilal <bsajid173@gmail.com>
2022-11-12 14:20:00 +0530

    projects section

---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,54 @@
+import BlogifyImage from "../../assets/images/Blogify.png";
+
+const Projects = [
+  {
+    id: 1,
+    title: "Blogify Web App",
+    description: "A blog website for tech junkies!!",
+    codeLink: "https://github.com/BilalSajid173/Blog-app-frontend",
+    projectLink: "https://blogify-app.vercel.app/",
+    img: BlogifyImage,
+  },
+  {
+    id: 1,
+    title: "Blogify",
+    description: "A blog website for tech junkies!!",
+    codeLink: "https://github.com/BilalSajid173/Blog-app-frontend",
+    projectLink: "https://blogify-app.vercel.app/",
+    img: BlogifyImage,
+  },
+  {
+    id: 1,
+    title: "Blogify",
+    description: "A blog website for tech junkies!!",
+    codeLink: "https://github.com/BilalSajid173/Blog-app-frontend",
+    projectLink: "https://blogify-app.vercel.app/",
+    img: BlogifyImage,
+  },
+  {
+    id: 1,
+    title: "Blogify",
+    description: "A blog website for tech junkies!!",
+    codeLink: "https://github.com/BilalSajid173/Blog-app-frontend",
+    projectLink: "https://blogify-app.vercel.app/",
+    img: BlogifyImage,
+  },
+  {
+    id: 1,
+    title: "Blogify",
+    description: "A blog website for tech junkies!!",
+    codeLink: "https://github.com/BilalSajid173/Blog-app-frontend",
+    projectLink: "https://blogify-app.vercel.app/",
+    img: BlogifyImage,
+  },
+  {
+    id: 1,
+    title: "Blogify",
+    description: "A blog website for tech junkies!!",
+    codeLink: "https://github.com/BilalSajid173/Blog-app-frontend",
+    projectLink: "https://blogify-app.vercel.app/",
+    img: BlogifyImage,
+  },
+];
+
+export default Projects;
--- src/components/Projects/Work.js
+++ src/components/Projects/Work.js
@@ -1,63 +1,96 @@
-import React, { useState } from "react";
-// import { AiFillEye, AiFillGithub } from "react-icons/ai";
+import React from "react";
+import LinkIcon from "@mui/icons-material/Link";
+import GitHubIcon from "@mui/icons-material/GitHub";
 import { motion } from "framer-motion";
-
-// import { AppWrap, MotionWrap } from "../../wrapper";
-// import { urlFor, client } from "../../client";
 import "./Work.scss";
-
-const works = [
-  {
-    projectLink: "https://www.google.com/",
-    codeLink: "https://www.github.com/",
-    title: "Blogify",
-    description: "blogify App",
-  },
-  {
-    projectLink: "https://www.google.com/",
-    codeLink: "https://www.github.com/",
-    title: "Blogify",
-    description: "blogify App",
-  },
-];
+import Projects from "./Projects";
 
 const Work = () => {
-  //   const [works, setWorks] = useState([]);
-  //   const [filterWork, setFilterWork] = useState([]);
-  //   const [activeFilter, setActiveFilter] = useState("All");
-  const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
-
-  //   useEffect(() => {
-  //     const query = '*[_type == "works"]';
-
-  //     client.fetch(query).then((data) => {
-  //       setWorks(data);
-  //       setFilterWork(data);
-  //     });
-  //   }, []);
-
-  //   const handleWorkFilter = (item) => {
-  //     setActiveFilter(item);
-  //     setAnimateCard([{ y: 100, opacity: 0 }]);
-
-  //     setTimeout(() => {
-  //       setAnimateCard([{ y: 0, opacity: 1 }]);
-
-  //       if (item === "All") {
-  //         setFilterWork(works);
-  //       } else {
-  //         setFilterWork(works.filter((work) => work.tags.includes(item)));
-  //       }
-  //     }, 500);
-  //   };
-
+  const cardVariants = {
+    offscreen: {
+      y: 300,
+    },
+    onscreen: {
+      y: 50,
+      transition: {
+        type: "spring",
+        bounce: 0.4,
+        duration: 0.8,
+      },
+    },
+  };
   return (
     <>
-      <h2 className="head-text">
-        My Creative <span>Portfolio</span> Section
+      <h2 className="font-bold text-center text-4xl dark:text-white my-[2rem]">
+        My Projects
       </h2>
+      <div className="app__work-portfolio">
+        {Projects.map((work, index) => (
+          <motion.div
+            // className="card-container"
+            initial="offscreen"
+            whileInView="onscreen"
+            viewport={{ once: true, amount: 0.8 }}
+          >
+            <motion.div className="card" variants={cardVariants}>
+              <div className="app__work-item app__flex" key={index}>
+                <div className="app__work-img app__flex">
+                  <img src={work.img} alt={work.title} />
 
-      {/* <div className="app__work-filter">
+                  <motion.div
+                    whileHover={{ opacity: [0, 1] }}
+                    transition={{
+                      duration: 0.25,
+                      ease: "easeInOut",
+                      staggerChildren: 0.5,
+                    }}
+                    className="app__work-hover app__flex"
+                  >
+                    <a href={work.projectLink} target="_blank" rel="noreferrer">
+                      <motion.div
+                        whileInView={{ scale: [0, 1] }}
+                        whileHover={{ scale: [1, 0.9] }}
+                        transition={{ duration: 0.25 }}
+                        className="app__flex"
+                      >
+                        <LinkIcon />
+                      </motion.div>
+                    </a>
+                    <a href={work.codeLink} target="_blank" rel="noreferrer">
+                      <motion.div
+                        whileInView={{ scale: [0, 1] }}
+                        whileHover={{ scale: [1, 0.9] }}
+                        transition={{ duration: 0.25 }}
+                        className="app__flex"
+                      >
+                        <GitHubIcon />
+                      </motion.div>
+                    </a>
+                  </motion.div>
+                </div>
+
+                <div className="app__work-content app__flex">
+                  <h2 className="bold-text">{work.title}</h2>
+                  <p className="p-text" style={{ marginTop: 10 }}>
+                    {work.description}
+                  </p>
+
+                  {/* <div className="app__work-tag app__flex">
+                <p className="p-text">{work.tags[0]}</p>
+              </div> */}
+                </div>
+              </div>
+            </motion.div>
+          </motion.div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default Work;
+
+/* <div className="app__work-filter">
         {["UI/UX", "Web App", "Mobile App", "React JS", "All"].map(
           (item, index) => (
             <div
@@ -71,74 +104,4 @@ const Work = () => {
             </div>
           )
         )}
-      </div> */}
-
-      <motion.div
-        animate={animateCard}
-        transition={{ duration: 2, delayChildren: 0.5 }}
-        className="app__work-portfolio"
-      >
-        {works.map((work, index) => (
-          <motion.div
-            whileInView={{ opacity: [0, 1] }}
-            transition={{
-              duration: 2.5,
-              ease: "easeInOut",
-              staggerChildren: 0.5,
-            }}
-          >
-            <div className="app__work-item app__flex" key={index}>
-              <div className="app__work-img app__flex">
-                {/* <img src={urlFor(work.imgUrl)} alt={work.name} /> */}
-
-                <motion.div
-                  whileHover={{ opacity: [0, 1] }}
-                  transition={{
-                    duration: 0.25,
-                    ease: "easeInOut",
-                    staggerChildren: 0.5,
-                  }}
-                  className="app__work-hover app__flex"
-                >
-                  <a href={work.projectLink} target="_blank" rel="noreferrer">
-                    <motion.div
-                      whileInView={{ scale: [0, 1] }}
-                      whileHover={{ scale: [1, 0.9] }}
-                      transition={{ duration: 0.25 }}
-                      className="app__flex"
-                    >
-                      Link
-                    </motion.div>
-                  </a>
-                  <a href={work.codeLink} target="_blank" rel="noreferrer">
-                    <motion.div
-                      whileInView={{ scale: [0, 1] }}
-                      whileHover={{ scale: [1, 0.9] }}
-                      transition={{ duration: 0.25 }}
-                      className="app__flex"
-                    >
-                      Github
-                    </motion.div>
-                  </a>
-                </motion.div>
-              </div>
-
-              <div className="app__work-content app__flex">
-                <h4 className="bold-text">{work.title}</h4>
-                <p className="p-text" style={{ marginTop: 10 }}>
-                  {work.description}
-                </p>
-
-                {/* <div className="app__work-tag app__flex">
-                <p className="p-text">{work.tags[0]}</p>
-              </div> */}
-              </div>
-            </div>
-          </motion.div>
-        ))}
-      </motion.div>
-    </>
-  );
-};
-
-export default Work;
+      </div> */
