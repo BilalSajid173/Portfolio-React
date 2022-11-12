de0d80fb63269fd389b55ba1a93db733421ac93b
Bilal <bsajid173@gmail.com>
2022-11-13 01:32:40 +0530

    footer done

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,40 @@
 import Waves from "./Waves";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import FacebookIcon from "@mui/icons-material/Facebook";
 
 const Footer = () => {
-  return <Waves />;
+  return (
+    <>
+      <Waves />
+      <div className="dark:bg-[#0a0c49] flex flex-wrap justify-center items-center py-10">
+        <div className="dark:text-gray-200">
+          <a
+            href="https://github.com/BilalSajid173"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <GitHubIcon fontSize="large" className="mx-2" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/bilal-sajid-5b1218219/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <LinkedInIcon fontSize="large" className="mx-2" />
+          </a>
+          <a
+            href="https://www.facebook.com/bilal.sajid.71697/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FacebookIcon fontSize="large" className="mx-2" />
+          </a>
+          <p className="mt-4">Created By Bilal Sajid ©</p>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Footer;
