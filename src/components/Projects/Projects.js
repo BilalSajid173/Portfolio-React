cb5730c25f8ce866264a580f5aac06826c0140be
Bilal <bsajid173@gmail.com>
2022-11-12 15:09:42 +0530

    projects section almost complete

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,53 +1,40 @@
 import BlogifyImage from "../../assets/images/Blogify.png";
+import KonnectImage from "../../assets/images/Konnect.png";
+import FoodbookImage from "../../assets/images/Foodbook.png";
+import PortfolioImage from "../../assets/images/Portfolio.png";
 
 const Projects = [
   {
     id: 1,
     title: "Blogify Web App",
-    description: "A blog website for tech junkies!!",
+    description: "A blog website for tech junkies!",
     codeLink: "https://github.com/BilalSajid173/Blog-app-frontend",
     projectLink: "https://blogify-app.vercel.app/",
     img: BlogifyImage,
   },
   {
-    id: 1,
-    title: "Blogify",
-    description: "A blog website for tech junkies!!",
-    codeLink: "https://github.com/BilalSajid173/Blog-app-frontend",
-    projectLink: "https://blogify-app.vercel.app/",
-    img: BlogifyImage,
+    id: 2,
+    title: "Konnect Web App",
+    description: "A Social Media app for everyone!",
+    codeLink: "https://github.com/BilalSajid173/chat-app-frontend",
+    projectLink: "https://konnect-social-app.vercel.app/",
+    img: KonnectImage,
   },
   {
-    id: 1,
-    title: "Blogify",
-    description: "A blog website for tech junkies!!",
-    codeLink: "https://github.com/BilalSajid173/Blog-app-frontend",
-    projectLink: "https://blogify-app.vercel.app/",
-    img: BlogifyImage,
+    id: 3,
+    title: "Recipe Web App",
+    description: "A foodbook app for foodies!",
+    codeLink: "https://github.com/BilalSajid173/foodbook",
+    projectLink: "https://fierce-waters-21031.herokuapp.com/",
+    img: FoodbookImage,
   },
   {
-    id: 1,
-    title: "Blogify",
-    description: "A blog website for tech junkies!!",
+    id: 4,
+    title: "Portfolio Site",
+    description: "Personal Website with framer-motion.",
     codeLink: "https://github.com/BilalSajid173/Blog-app-frontend",
     projectLink: "https://blogify-app.vercel.app/",
-    img: BlogifyImage,
-  },
-  {
-    id: 1,
-    title: "Blogify",
-    description: "A blog website for tech junkies!!",
-    codeLink: "https://github.com/BilalSajid173/Blog-app-frontend",
-    projectLink: "https://blogify-app.vercel.app/",
-    img: BlogifyImage,
-  },
-  {
-    id: 1,
-    title: "Blogify",
-    description: "A blog website for tech junkies!!",
-    codeLink: "https://github.com/BilalSajid173/Blog-app-frontend",
-    projectLink: "https://blogify-app.vercel.app/",
-    img: BlogifyImage,
+    img: PortfolioImage,
   },
 ];
 
--- src/components/Projects/Work.js
+++ src/components/Projects/Work.js
@@ -11,10 +11,10 @@ const Work = () => {
       y: 300,
     },
     onscreen: {
-      y: 50,
+      y: 0,
       transition: {
         type: "spring",
-        bounce: 0.4,
+        bounce: 0.5,
         duration: 0.8,
       },
     },
@@ -24,18 +24,24 @@ const Work = () => {
       <h2 className="font-bold text-center text-4xl dark:text-white my-[2rem]">
         My Projects
       </h2>
-      <div className="app__work-portfolio">
+      <div className="flex flex-wrap justify-center items-center">
         {Projects.map((work, index) => (
           <motion.div
-            // className="card-container"
             initial="offscreen"
             whileInView="onscreen"
             viewport={{ once: true, amount: 0.8 }}
           >
-            <motion.div className="card" variants={cardVariants}>
-              <div className="app__work-item app__flex" key={index}>
-                <div className="app__work-img app__flex">
-                  <img src={work.img} alt={work.title} />
+            <motion.div variants={cardVariants}>
+              <div
+                className="flex flex-col w-[330px] m-[2rem] p-[1rem] rounded-sm bg-gray-200 dark:bg-[#372656] hover:shadow-lg dark:text-gray-200 transition-all duration-300 ease-in"
+                key={index}
+              >
+                <div className="w-full h-[230px] relative flex justify-center items-center">
+                  <img
+                    src={work.img}
+                    alt={work.title}
+                    className="w-full h-full rounded-sm object-cover"
+                  />
 
                   <motion.div
                     whileHover={{ opacity: [0, 1] }}
@@ -44,14 +50,14 @@ const Work = () => {
                       ease: "easeInOut",
                       staggerChildren: 0.5,
                     }}
-                    className="app__work-hover app__flex"
+                    className="app__work-hover flex justify-center items-center"
                   >
                     <a href={work.projectLink} target="_blank" rel="noreferrer">
                       <motion.div
                         whileInView={{ scale: [0, 1] }}
                         whileHover={{ scale: [1, 0.9] }}
                         transition={{ duration: 0.25 }}
-                        className="app__flex"
+                        className="flex justify-center items-center"
                       >
                         <LinkIcon />
                       </motion.div>
@@ -61,7 +67,7 @@ const Work = () => {
                         whileInView={{ scale: [0, 1] }}
                         whileHover={{ scale: [1, 0.9] }}
                         transition={{ duration: 0.25 }}
-                        className="app__flex"
+                        className="flex justify-center items-center"
                       >
                         <GitHubIcon />
                       </motion.div>
@@ -69,15 +75,11 @@ const Work = () => {
                   </motion.div>
                 </div>
 
-                <div className="app__work-content app__flex">
-                  <h2 className="bold-text">{work.title}</h2>
+                <div className="flex justify-center items-center flex-col w-full relative p-[0.5rem]">
+                  <h2 className="mt-[1rem] font-bold text-lg">{work.title}</h2>
                   <p className="p-text" style={{ marginTop: 10 }}>
                     {work.description}
                   </p>
-
-                  {/* <div className="app__work-tag app__flex">
-                <p className="p-text">{work.tags[0]}</p>
-              </div> */}
                 </div>
               </div>
             </motion.div>
@@ -89,19 +91,3 @@ const Work = () => {
 };
 
 export default Work;
-
-/* <div className="app__work-filter">
-        {["UI/UX", "Web App", "Mobile App", "React JS", "All"].map(
-          (item, index) => (
-            <div
-              key={index}
-              onClick={() => handleWorkFilter(item)}
-              className={`app__work-filter-item app__flex p-text ${
-                activeFilter === item ? "item-active" : ""
-              }`}
-            >
-              {item}
-            </div>
-          )
-        )}
-      </div> */
